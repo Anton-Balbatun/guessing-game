624e864854b7869eab975c73c977200f4dc1cc27
Anton Balbatun <anton553322@gmail.com>
2019-10-10 18:09:43 +0300

    feat: do task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        
+        this.guessNumber = 0
+        this.min = 0
+        this.max = 0
+    }
 
     setRange(min, max) {
-
+            this.min = min
+            this.max = max
     }
 
     guess() {
-
+         this.guessNumber =  Math.round((this.max + this.min)/2)
+           // console.log( this.guessNumber)
+            return this.guessNumber
+        
     }
 
     lower() {
-
+       
+            this.max = this.guessNumber
+        
     }
 
     greater() {
-
+        
+            this.min = this.guessNumber
+        
     }
 }
 
